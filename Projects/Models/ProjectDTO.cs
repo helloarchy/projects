1c6f364ecc6245362a3515fecf60f3043dfc147a
Robert Hardy <helloarchy@gmail.com>
2021-10-17 17:37:06 +0100

    Update projects api to include a Project model, DTO, and controller.

---
+++ Projects/Models/ProjectDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Projects.Models
+{
+    public class ProjectDTO
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
--- Projects/Startup.cs
+++ Projects/Startup.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+using Projects.Models;
 
 namespace Projects
 {
@@ -28,6 +24,9 @@ namespace Projects
         {
 
             services.AddControllers();
+
+            services.AddDbContext<ProjectContext>(options => options.UseInMemoryDatabase("Projects"));
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Projects", Version = "v1" });

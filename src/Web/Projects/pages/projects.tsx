dbca51a1d90c92141f57889f4ebff6c4e911c90e
Robert Hardy <hardyrar@gmail.com>
2022-04-22 20:51:30 +0100

    List project titles if any in response

--- src/Web/Projects/pages/projects.tsx
+++ src/Web/Projects/pages/projects.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link'
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import Link from "next/link";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 
 type Project = {
   title: string;
@@ -13,24 +13,40 @@ type Data = {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // Fetch data from external API
-  const res = await fetch(`http://localhost:6000/api/projects`)
-  const data: Data = await res.json()
+  const res = await fetch(`http://localhost:5000/api/projects`);
+  const data: Data = await res.json();
 
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data } };
+};
+
+function renderProjects(projects: Project[]) {
+  {
+    if (projects?.length) {
+      return (
+        <ul>
+          {projects.map((project: Project) => (
+            <li key={project.title}>{project.title}</li>
+          ))}
+        </ul>
+      );
+    } else {
+      return <p>No projects</p>;
+    }
+  }
 }
 
 const ProjectsPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
-  <Layout title='Projects | All Projects'>
+  <Layout title="Projects | All Projects">
     <h1>Projects</h1>
     <p>This is the page for all projects</p>
-    {}
+    {renderProjects(data.projects)}
     <p>
-      <Link href='/'>
+      <Link href="/">
         <a>Go home</a>
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default ProjectsPage
+export default ProjectsPage;

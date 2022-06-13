2050eb83005bacb97447128a14edd9cd43495505
Robert Hardy <hardyrar@gmail.com>
2022-06-13 20:50:03 +0100

    Let database set PK on Project

--- src/Services/Project/Project.API/Models/Project.cs
+++ src/Services/Project/Project.API/Models/Project.cs
@@ -2,7 +2,7 @@
 
 public class Project
 {
-    public Guid Id { get; } = new ();
+    public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public DateTime Created { get; set; }

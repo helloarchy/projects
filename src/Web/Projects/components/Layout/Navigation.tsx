dab3b021946fccf4c3f10343c456abc252c62d7f
Robert Hardy <hardyrar@gmail.com>
2022-07-04 19:42:00 +0100

    Use button group for nav

--- src/Web/Projects/components/Layout/Navigation.tsx
+++ src/Web/Projects/components/Layout/Navigation.tsx
@@ -1,24 +1,25 @@
+import { Button } from '@nextui-org/react'
 import Link from 'next/link'
 import React from 'react'
 
 const Navigation = () => {
   return (
-    <>
-      <nav>
-        <Link href="/pages">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/pages/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/pages/projects">
-          <a>Projects List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </>
+    <nav>
+      <Button.Group
+        color="gradient"
+        ghost
+      >
+        <Link href="/">
+          <Button>Home</Button>
+        </Link>
+        <Link href="/about">
+          <Button>About</Button>
+        </Link>
+        <Link href="/projects">
+          <Button>Projects</Button>
+        </Link>
+      </Button.Group>
+    </nav>
   )
 }
 
